6587e8f86434514cc3f9518061835e119080d946
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2022-06-06 19:49:32 -0300

    criei um arquivo novo

---
+++ jscript.js
@@ -0,0 +1,6 @@
+var nome = "Tatiana"; 
+var sobrenome = "Fernandes";
+var minhaprofissao = "Advogada";
+//console.log(nome + "" + sobrenome + profissao);
+
+console.log(`${nome} ${sobrenome} ${ minhaprofissao}`);
