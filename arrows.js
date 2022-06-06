0834b5bd9619cf34085065ab314cbff44db9c6d0
Tatiana Pauline Fernandes <79177501+pauline40-ss@users.noreply.github.com>
2022-06-06 20:26:00 -0300

    inclusão do arquivo arrows.js

---
+++ arrows.js
@@ -0,0 +1,12 @@
+function soma (x,y){
+    return x + y;
+}
+const multiplicacao = function(x,y){
+    return x*y
+};
+console.log(multiplicacao(5,8));
+
+const divisão = function(x,y){
+    return x / y
+};
+console.log(divisão(40,8));
